aaabc8598a1f1f5f4e6af9500544688f52a7e036
coderhhan <906271803@qq.com>
2023-12-30 19:31:34 +0800

    ceshi

--- packages/hooks/src/useUnmount/demo/demo1.tsx
+++ packages/hooks/src/useUnmount/demo/demo1.tsx
@@ -3,7 +3,7 @@
  * desc: 在组件卸载时，执行函数。
  */
 
-//22import { useBoolean, useUnmount } from 'uni-hooks';
+//222import { useBoolean, useUnmount } from 'uni-hooks';
 import { message } from 'antd';
 import React from 'react';
 
